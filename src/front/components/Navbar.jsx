35c6abb1e905c4f03ab32780919f8c60a2be226a
Gonzalo Triviño <gonzalotcp@gmail.com>
2025-10-08 18:01:46 +0000

    Lógica Navbar planteada, falta unir con rutas y pages de Brian para ver si funciona

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,17 +1,45 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Navbar = () => {
+
+	const [isLogged, setIsLogged] = useState(false);
+	const { store, dispatch } = useGlobalReducer();
+	const [perfil, setPerfil] = useState (null);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (store.perfilUsuario || store.perfilUsuario.token) {
+			setIsLogged(true);
+			setPerfil(store.perfilUsuario);
+		} else {
+			setIsLogged(false);
+			setPerfil(null);
+		}
+			
+		}, [store.perfilUsuario]);
+
+		const handleLogOut = () => {
+			localStorage.removeItem("token");
+			setIsLogged(false);
+			navigate("/");
+		}
 
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">Rute Zero</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+					{isLogged ? ( 
+						<button className="btn btn-danger" onClick={handleLogOut}>LogOut</button>
+					) : (
+					<Link to="/login">
+						<button className="btn btn-primary">LogIn</button>
 					</Link>
+					)}
 				</div>
 			</div>
 		</nav>
